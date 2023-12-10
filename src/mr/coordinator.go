915f48bbefb40c843a200bcd39622447d57f891b
sweetyhxt <112451024+sweetyhxt@users.noreply.github.com>
2023-12-10 21:08:39 +0800

    想到的第一个解决方案，后续按照思路解决

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -6,6 +6,7 @@ import "os"
 import "net/rpc"
 import "net/http"
 
+// todo 暂时想到需要添加一个任务桶，一个已分配待完成桶，两个桶都需要添加锁进行控制，
 type Coordinator struct {
 	// Your definitions here.
 
@@ -20,6 +21,10 @@ func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
 	reply.Y = args.X + 1
 	return nil
 }
+
+// todo 需要添加一个服务函数，启动一个协程，协程运行具体分配任务函数
+
+// todo 分配任务函数，分配任务后沉睡10s，之后监测任务是否已经完成，如果未完成还需要将任务重新加入任务队列
 
 // start a thread that listens for RPCs from worker.go
 func (c *Coordinator) server() {
@@ -42,7 +47,7 @@ func (c *Coordinator) Done() bool {
 
 	// Your code here.
 
-	// todo
+	// todo 需要检测任务是否全部完成，完成后返回true
 	return ret
 }
 
