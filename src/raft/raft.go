7093018dc1b5f95cd86f705a821971d1ea72d6c1
sweetyhxt <112451024+sweetyhxt@users.noreply.github.com>
2024-02-27 12:18:16 +0800

    修改代码

--- src/raft/raft.go
+++ src/raft/raft.go
@@ -257,7 +257,10 @@ func (rf *Raft) ReceiveInstructRPC(args *AppendEntriesArgs, reply *AppendEntries
 // that the caller passes the address of the reply struct with &, not
 // the struct itself.
 func (rf *Raft) sendRequestVote(server int, args *RequestVoteArgs, reply *RequestVoteReply) (bool, int) {
-	rf.peers[server].Call("Raft.RequestVote", args, reply)
+	ok := rf.peers[server].Call("Raft.RequestVote", args, reply)
+	if !ok {
+		fmt.Println(strconv.Itoa(server) + "断线了")
+	}
 	if reply.VoteGranted {
 		fmt.Println(strconv.Itoa(server) + "的投票同意")
 	} else {
