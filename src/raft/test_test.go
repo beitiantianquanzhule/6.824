d30462af2a6fee82a67c861b8496f9f15ad3d9db
sweetyhxt <112451024+sweetyhxt@users.noreply.github.com>
2024-02-27 16:54:01 +0800

    修改代码

--- src/raft/test_test.go
+++ src/raft/test_test.go
@@ -67,7 +67,7 @@ func TestReElection2A(t *testing.T) {
 	// disturb the new leader.
 	cfg.connect(leader1)
 	leader2 := cfg.checkOneLeader()
-
+	fmt.Println("2222222222222222222222222222222222222222222222222222222222222222222")
 	// if there's no quorum, no leader should
 	// be elected.
 	cfg.disconnect(leader2)
